48656714df27ccbac2aa06386086e2eafd2f3687
Jet Lee <asanofelipe@gmail.com>
2023-04-10 19:01:44 -0300

    primeiro commit

--- arquivo.h
+++ arquivo.h
@@ -10,11 +10,18 @@ using namespace std;
 #ifndef ARQUIVO_H
 #define ARQUIVO_H
 
+// classe criado para realizar a leitura do arquivo texto
+// contendo os dados do grafo
 class Arquivo {
     private:
         string nomeArq;
     public:
+        // Pre-Condicao: Nenhum
+        // Pos-Condicao: classe arquivo recebe o nome do arquivo a ser lido
         Arquivo(string nomeArq);
+
+        //Pre-Condicao: Nenhum
+        //Pos-Condicao: Grafo eh lido
         Grafo lerGrafo();
 };
 
